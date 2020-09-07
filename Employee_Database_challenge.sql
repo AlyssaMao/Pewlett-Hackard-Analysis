be95139fbbdf19f2ae4f59d52182c0f9b50bfe35
Alyssa Mao <alyssa.mao@salesforce.com>
2020-09-07 15:58:17 -0700

    second push

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -27,3 +27,20 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT (title) DESC;
+
+-- DELIVERABLE 2
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date, 
+	de.from_date,
+	de.to_date,
+	tit.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as tit
+ON (e.emp_no = tit.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
